e8a631a4d158ba2d11d392350dcb78f79d96ff71
VascoAmaral9 <vasco_amaral96@hotmail.com>
2019-01-21 03:35:39 +0000

    ..

--- WebPlatform/client/scripts/layout.js
+++ WebPlatform/client/scripts/layout.js
@@ -29379,8 +29379,8 @@ module.exports = function () {
             building_range: 90,
             user_range: 150,
             timeout_updateLocation: 1, // User periodic location update -- 1 sec
-            timeout_activeUser: 3, // User considered inactive -- 3 sec
-            timeout_checkGlobal: 1 // Periodic update on active users -- 1 sec
+            timeout_activeUser: 5, // User considered inactive -- 5 sec
+            timeout_checkGlobal: 60 // Periodic update on active users -- 1 min
         }
     };
 
